436a78caa1cb0bde4673f330bdf97dd03ef899a4
Nate Jensen <nathan.jensen.58@gmail.com>
2018-04-22 11:27:05 -0600

    5_dodge_em

---
+++ 5_dodge_em/README.md
@@ -0,0 +1,3 @@
+This can be played on https://dodge-em.surge.sh
+
+A dodge-ball-esque mini-game.  This works best on computers as I'm still learning touch events.  The ball and the enemies are two classes, where the enemies move() method handles it's movement regardless of where it's initially placed, and will detect whether the user's circle has made contact.
--- 5_dodge_em/app.js
+++ 5_dodge_em/app.js
@@ -17,6 +17,8 @@ let enemyTimerID;
 let scoreCounID;
 let score = 0;
 
+
+// Initial game button
 const makeStartButton = btn => {
         btn.innerHTML = `<span>Click Me</span>`;
         btn.classList.add('startBtn');
@@ -24,17 +26,19 @@ const makeStartButton = btn => {
         return btn;
 }
 
+
+// Score keeping functions
 const scoreKeeper = () => {
      score++;
      scoreSpan.textContent = score;
 }
-
 const scoreCounter = () => {
     scoreCountID = setInterval(scoreKeeper, 100);
 }
 
 
 
+// new ball created at every re render with x and y on mouse position;
 class Ball {
     constructor(x, y) {
         this.x = x;
@@ -52,6 +56,7 @@ class Ball {
 
 
 
+
 class Enemy {
     constructor(x, y, v, d, forwardX, forwardY){
         this.x = x;
@@ -61,6 +66,8 @@ class Enemy {
         this.forwardX = true;
         this.forwardY = true;
         this.move = () => {
+
+            // Handles X and Y movement regardless of initial placement
             if(this.x <= canvas.width  && forwardX){
                 x += v;
             } else if(this.x > canvas.width || !forwardX){
@@ -84,7 +91,7 @@ class Enemy {
                 }
             }
 
-            //
+            // print enemy to canvas
             ctx.save();
                 ctx.fillStyle = 'firebrick';
                 ctx.beginPath();
@@ -96,43 +103,87 @@ class Enemy {
     }
 }
 
+
+// Big messy function that creates random enemy starting positions, speed, and chance.
 const enemyOptionChooser = () => {
-    let randomNum = Math.floor(Math.random() * (100 - 1) + 1);
+    let randomNum = Math.floor(Math.random() * (315 - 1) + 1);
     let opt1 = {
-        x: Math.floor(Math.random() * (w - 10)  + 10),
+        x: Math.floor(Math.random() * ((w - w/2) - 10)  + 10),
         y: -20,
         v: Math.floor(Math.random() * (20 - 1) + 1),
         d: 3
     }
     let opt2 = {
-        x: Math.floor(Math.random() * (w - 10)  + 10),
+        x: Math.floor(Math.random() * ((w - w/2) - 10)  + 10),
         y: h + 20,
-        v: Math.floor(Math.random() * (10- 1) + 1),
-        d: Math.floor(Math.random() * (10 - 1) + 1)
+        v: Math.floor(Math.random() * (15- 1) + 1),
+        d: Math.floor(Math.random() * (15 - 1) + 1)
     }
     let opt3 = {
         x: Math.floor(Math.random() * (w - 100)  + 100),
         y: h + 20,
         v: 0,
-        d: Math.floor(Math.random() * (10 - 1) + 1)
+        d: Math.floor(Math.random() * (10 - 5) + 5)
     }
     let opt4 = {
         x: -20,
-        y: Math.floor(Math.random() * (h - 50) + 50),
-        v: Math.floor(Math.random() * (10 - 1) + 1),
-        d: Math.floor(Math.random() * (10 - 1) + 1)
+        y: Math.floor(Math.random() * (h / 2 - 50) + 50),
+        v: Math.floor(Math.random() * (20 - 1) + 1),
+        d: Math.floor(Math.random() * (20 - 5) + 5)
     }
     let opt5 = {
         x: w + 20,
         y: Math.floor(Math.random() * ((h + 30) - 20) + 20),
-        v: Math.floor(Math.random() * (10 - 1) + 1),
-        d: Math.floor(Math.random() * (10 - 1) + 1)
+        v: 4,
+        d: Math.floor(Math.random() * (20 - 1) + 1)
     }
     let opt6 = {
-        x: w + 20,
-        y: Math.floor(Math.random() * ((h + 30) - 20) + 20),
-        v: Math.floor(Math.random() * (10 - 1) + 1),
+        x: Math.floor(Math.random() * (w / 2) + 20),
+        y: -20,
+        v: Math.floor(Math.random() * (15 - 5) + 5),
         d: 0
+    }
+    let opt7 = {
+        x: w + 20,
+        y: Math.floor(Math.random() * (h  - 20) + 20),
+        v: 0,
+        d: Math.floor(Math.random() * (15 - 1) + 1)
+    }
+    let opt8 = {
+        x: w + 20,
+        y: Math.floor(Math.random() * ((h + 100)  - 40) + 40),
+        v: Math.floor(Math.random() * (15 - 5) + 1),
+        d: Math.floor(Math.random() * (15 - 5) + 1)
+    }
+    let opt9 = {
+        x: Math.floor(Math.random() * (w/2 - 50) + 80),
+        y: - 20,
+        v: 0,
+        d: Math.floor(Math.random() * (15 - 5) + 1)
+    }
+    let opt10 = {
+        x: w + 20,
+        y: Math.floor(Math.random() * (h - 10) + 10),
+        v: Math.floor(Math.random() * (8 - 2) + 2),
+        d: 0
+    }
+    let opt11 = {
+        x: -20,
+        y: Math.floor(Math.random() * (h - 40) + 40),
+        v: Math.floor(Math.random() * (8 - 2) + 2),
+        d: 0
+    }
+    let opt12 = {
+        x: Math.floor(Math.random() * (w - 20) + 20),
+        y: h + 20,
+        v: 0,
+        d: Math.floor(Math.random() * (8 - 2) + 2)
+    }
+    let opt13 = {
+        x: Math.floor(Math.random() * (w - 20) + 20),
+        y: -20,
+        v: Math.floor(Math.random() * (15 - 5) + 5),
+        d: Math.floor(Math.random() * (25 - 20) + 20)
     }
     if(randomNum < 15){
         return opt1
@@ -144,11 +195,27 @@ const enemyOptionChooser = () => {
         return opt4
     } else if(randomNum >= 70 && randomNum < 85){
         return opt5;
-    } else if(randomNum >= 85 && randomNum <= 100){
+    } else if(randomNum >= 85 && randomNum < 100){
         return opt6;
+    } else if(randomNum >= 100 && randomNum < 120){
+        return opt7;
+    } else if(randomNum >= 120 && randomNum < 140){
+        return opt8;
+    } else if(randomNum >= 140 && randomNum < 180){
+        return opt9;
+    } else if(randomNum >= 180 && randomNum < 225){
+        return opt10;
+    } else if(randomNum >= 225 && randomNum < 270){
+        return opt11;
+    } else if(randomNum >= 270 && randomNum < 300){
+        return opt12;
+    } else if(randomNum >= 300 && randomNum < 315){
+        return opt13;
     }
 }
 
+
+// creat enemey and random option, and push to array so clock() can use enemy.move()
 const createEnemy = () => {
     let enemy;
     let opt = enemyOptionChooser();
@@ -156,15 +223,15 @@ const createEnemy = () => {
     enemies.push(enemy)
 }
 
+// new ball on every render
 const createBall = (x, y) => {
     let ball = new Ball(x, y)
-    balls.push(ball);
 }
 
 
-
+// generates an enemy once every .1 second// could expand this to decrement when score hits certain threshholds
 const enemyTimer = () => {
-    enemyTimerID = setInterval(createEnemy, 250);
+    enemyTimerID = setInterval(createEnemy, 100);
 }
 
 const start = () => {
@@ -179,9 +246,25 @@ const start = () => {
         scoreCounter();
 }
 
+const endGame = () => {
+    ctx.clearRect(0, 0, w, h);
+    body.classList.remove('removeCursor');
+    canvas.style.backgroundColor = "#111";
+    let text = `Final Score: ${score}`
+    ctx.font = '30px sans-serif';
+    ctx.fontWeight = 300;
+    ctx.fillStyle = 'white';
+    ctx.fillText(text, w/3, h/2 - 20);
+}
 
 const clock = () => {
     ctx.clearRect(0, 0, w, h);
+    if(livesCount <= 0){
+        clearInterval(startID);
+        clearInterval(enemyTimerID);
+        clearInterval(scoreCountID);
+        endGame()
+    }
     createBall(userMouse.x, userMouse.y)
     if(enemies){
         for(let i = 0; i < enemies.length; i++){
@@ -198,6 +281,18 @@ body.appendChild(makeStartButton(btn));
 window.addEventListener('mousemove', (e) => {
     userMouse.x = e.offsetX;
     userMouse.y = e.offsetY;
+})
+
+window.addEventListener('touchstart', (e) => {
+    e.preventDefault();
+    userMouse.x = e.pageX;
+    userMouse.y = e.pageY;
+})
+
+window.addEventListener('touchmove', (e) => {
+    e.preventDefault();
+    userMouse.x = e.pageX;
+    userMouse.y = e.pageY;
 })
 
 window.addEventListener('resize', () => {
